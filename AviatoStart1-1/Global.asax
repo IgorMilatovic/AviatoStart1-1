15b439758e4c938ca041487a8c49c8298704b1d9
IgorMilatovic <igby.veko@gmail.com>
2019-04-09 12:56:16 +0200

    Add project files.

---
+++ AviatoStart1-1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AviatoStart1_1.MvcApplication" Language="C#" %>
---
+++ AviatoStart1-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AviatoStart1_1.Startup))]
+namespace AviatoStart1_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
